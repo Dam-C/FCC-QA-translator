1e4a1be0f99302f62a655a3f34586405ae85f3e9
Damien Charlois <DamienC@macbook-pro-de-joelle.home>
2024-06-19 14:16:15 +0200

    updated translator

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,11 @@
+const { match } = require('assert');
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require('./british-only.js');
 
 const reverseDict = (obj) => {
-  return Object.assign({}, ...Object.entries(obj).map(([k,v]) => ({[v]: k})));
+  return Object.fromEntries(Object.entries(obj).map(([k,v]) => [v, k]));
 }
 
 class Translator {
@@ -24,59 +25,48 @@ class Translator {
   }
 
   translate(text, dict, titles, timeRegex, locale) {
+    let finalRet;
     const lowTxt = text.toLowerCase();
-    const matchesMap = {};
-
+    const matchesMap = new Map();
     // check titles
-    Object.entries(titles).forEach(([k, v]) => {
-      if (lowTxt.includes(k)) {
-        matchesMap[k] = v[0].toUpperCase() + v.slice(1);
-      }
-    });
-
-    // check spaced words
-    const spacedWords = Object.fromEntries(
-      Object.entries(dict).filter(([k, v]) => k.includes(' '))
-    );
-    
-    Object.entries(spacedWords).forEach(([k, v]) => {
-      if (lowTxt.includes(k)) {
-        matchesMap[k] = v;
-      }
-    });
-
-    // words matches
-    lowTxt.match(/(\w+([-'](\w+)?)?)/g).forEach((w) => {
-      if (dict[w]) matchesMap[w] = dict[w];
-    });
-
-    // time matches
-    const matches = text.match(timeRegex);
-    if (matches) {
-      matches.forEach((e) => {
-        if (locale === 'toUK') {
-          matchesMap[e] = e.replace(':', '.');
-        } else {
-          matchesMap[e] = e.replace('.', ':');
+    for (const [k,v] of Object.entries(titles)) {
+        if (lowTxt.includes(k)) {
+            matchesMap.set(k, v.charAt(0).toUpperCase() + v.slice(1));
         }
-      });
     }
-
-    if (Object.keys(matchesMap).length === 0) return [text, text];
-
-    const translation = this.replace(text, matchesMap);
-    const translationHL = this.replaceWithHighlight(text, matchesMap);
-    return [translation, translationHL];
+    // check spaced words
+    for (const [k, v] of Object.entries(dict)) {
+        if (k.includes(' ') && lowTxt.includes(k)) {
+            matchesMap.set(k, v);
+        }
+    }
+    // words matches
+    for (const w of lowTxt.match(/(\w+([-'](\w+)?)?)/g) || []) {
+        if (dict[w]) matchesMap.set(w, dict[w]);
+    }
+    // time matches
+    for (const m of text.match(timeRegex) || []) {
+        matchesMap.set(m, locale === 'toUK' ? m.replace(':', '.') : m.replace('.', ':'))
+    }
+    if (matchesMap.size === 0) {
+        finalRet = [text, text]
+    } else {
+        finalRet = [ 
+            this.replace(text, matchesMap), 
+            this.replaceWithHighlight(text, matchesMap)
+        ]
+    };
+    return finalRet;
   }
 
   replace(text, matches) {
-    const re = new RegExp(Object.keys(matches).join('|'), 'gi');
-    return text.replace(re, (m) => matches[m.toLowerCase()]);
+    const re = new RegExp([...matches.keys()].join('|'), 'gi');
+    return text.replace(re, (m) => matches.get(m.toLowerCase()));
   }
   
   replaceWithHighlight(text, matches) {
-    const re = new RegExp(Object.keys(matches).join('|'), 'gi');
-    return text.replace(re, (m) => `<span class="highlight">${matches[m.toLowerCase()]}</span>`);
+    const re = new RegExp([...matches.keys()].join('|'), 'gi');
+    return text.replace(re, (m) => `<span class="highlight">${matches.get(m.toLowerCase())}</span>`);
   }
 }
 
